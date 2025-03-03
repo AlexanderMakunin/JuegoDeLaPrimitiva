b8470cd31704c078983f6275c0025919207c893c
Javi <javiermengual@live.com>
2025-03-03 17:35:39 +0100

    todo funcionando

--- src/main/java/com/primitiva/Main.java
+++ src/main/java/com/primitiva/Main.java
@@ -2,7 +2,7 @@ package com.primitiva;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("hola");
 
     }
 }
