faf0794d6975bf99f2a03981cd18eb7e1cf077a4
Javi <114607632+JavMB@users.noreply.github.com>
2025-03-03 12:08:23 +0100

    como no vaya ahora paso

--- src/main/java/com/primitiva/Main.java
+++ src/main/java/com/primitiva/Main.java
@@ -1,7 +1,7 @@
 package com.primitiva;
 
 public class Main {
-    public static void mai(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) {
+        System.out.pritln("Hello world!");
     }
 }
