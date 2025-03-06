b49c2b6f712a2fcabfadc3a26d622fd8db1a3c82
Alexander Makunin <amakunin7@gmail.com>
2025-03-06 21:17:30 +0100

    comentar todo y no pete al meter double/float

--- src/main/java/com/primitiva/Main.java
+++ src/main/java/com/primitiva/Main.java
@@ -9,14 +9,10 @@ public class Main {
     private static final Random aleatorio = new Random();
     // Array de los numeros
     private final static int[] num = new int[6];
-    // numero de re integro
-    private static int numReintegro = 0;
     // numero minimo de la suerte
     private static final int numMin = 1;
     // numero maximo de la suerte
     private static final int numMax = 49;
-    // numero maximo de re integro
-    private static final int numMaxReintegro = 10;
     // String para que el usuario introduzca los datos
     private static int numIntroducido;
     // Scanner para leer los datos
@@ -32,27 +28,31 @@ public class Main {
      */
     private static void crearBoleto(){
         System.out.println("*** Hagamos sus numeros de la suerte ***");
+        // Pedir al usuario si lo hace a mano o aleatorio
         System.out.println("Quieres introducirlos a mano? (S/N)");
         char numerosDeLaSuerte = leer.nextLine().charAt(0);
         if (numerosDeLaSuerte == 's' || numerosDeLaSuerte == 'S') {
+            // en caso de manual ir pidiendo varias veces hasta la longitud de
             for (int i = 0; i < num.length; i++) {
                 System.out.println("Indique su numero de la suerte num (entre 1 al 49): " + (i+1));
-                numIntroducido = Integer.parseInt(leer.nextLine());
+                // pa que si pongas un float o double no pete
+                numIntroducido = (int) Double.parseDouble(leer.nextLine());
+                // verificar si los numeros estan dentro del rango
                 while (numIntroducido > numMax || numIntroducido < numMin) {
                     System.err.println("El numero introducido no es valido: " + numIntroducido + ", vuelve a intentarlo");
                 }
+                // meter los numeros en el array en la cual es el boleto
                 num[i] = numIntroducido;
             }
         } else {
+            // por si a la persona le da flojera hacerlo a mano
             for (int i = 0; i < num.length; i++) {
                 num[i] = aleatorio.nextInt(numMin,numMax+1);
             }
         }
-        numReintegro = aleatorio.nextInt(numMin,numMaxReintegro+1);
+        // mostar el boleto
         System.out.println("*** Estos son tus numeros de la suerte ***");
         System.out.println(Arrays.toString(num));
-        System.out.println("*** Este es tu numero de reintegro ***");
-        System.out.println(numReintegro);
     }
     /*
     El menu para llamar los metodos de los juegos
@@ -68,7 +68,6 @@ public class Main {
             System.out.println("*** 5. Jugar hasta obtener premio categoría especial ***");
             System.out.println("*** 0. Salir ***");
             numIntroducido = Integer.parseInt(leer.nextLine());
-
             /* Aqui van los metodos de los juegos
             switch (numIntroducido) {
                 case 1 ->
